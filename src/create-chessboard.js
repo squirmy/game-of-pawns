ee4577c23719b09b94f1550b8154a7fff03db315
Adam Woods <adam.tomato@gmail.com>
2019-04-14 00:22:37 +0800

    remove unnecessary map/flatmap

--- src/create-chessboard.js
+++ src/create-chessboard.js
@@ -1,13 +1,8 @@
 export default piecePlacement => {
-  return piecePlacement
-    .split('/')
-    .map(rank => {
-      return [...rank]
-        .map(placement => {
-          const emptySquares = parseInt(placement, 10);
-          return emptySquares ? Array(emptySquares).fill('.') : [placement];
-        })
-        .flatMap(x => x);
-    })
-    .flatMap(x => x);
+  return piecePlacement.split('/').flatMap(rank => {
+    return [...rank].flatMap(placement => {
+      const emptySquares = parseInt(placement, 10);
+      return emptySquares ? Array(emptySquares).fill('.') : [placement];
+    });
+  });
 };
