67050216539e4306567610e960dd7e5ec09bba32
Adam Woods <adam.tomato@gmail.com>
2019-04-13 20:21:31 +0800

    readability maybe

--- src/generate-chessboard.js
+++ src/generate-chessboard.js
@@ -13,22 +13,24 @@ export default chooseNumberOfPieces => {
 
   const placeOne = piece => times(1, () => partial(placePieceRandomly, piece));
 
-  const placePieces = shuffle(
-    [
-      placeOne('K'),
-      placeOne('k'),
-      placeN(1, 'Q'),
-      placeN(1, 'q'),
-      placeN(8, 'P'),
-      placeN(8, 'p'),
-      placeN(2, 'R'),
-      placeN(2, 'r'),
-      placeN(2, 'N'),
-      placeN(2, 'n'),
-      placeN(2, 'B'),
-      placeN(2, 'b')
-    ].flatMap(x => x)
+  const placePieces = flow(
+    shuffle(
+      [
+        placeOne('K'),
+        placeOne('k'),
+        placeN(1, 'Q'),
+        placeN(1, 'q'),
+        placeN(8, 'P'),
+        placeN(8, 'p'),
+        placeN(2, 'R'),
+        placeN(2, 'r'),
+        placeN(2, 'N'),
+        placeN(2, 'n'),
+        placeN(2, 'B'),
+        placeN(2, 'b')
+      ].flatMap(x => x)
+    )
   );
 
-  return flow(placePieces)(createChessboard('8/8/8/8/8/8/8/8'));
+  return placePieces(createChessboard('8/8/8/8/8/8/8/8'));
 };
