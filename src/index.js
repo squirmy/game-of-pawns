37ff2dbed434818cfe9f5e7c23acf87faa721c27
Adam Woods <adam.tomato@gmail.com>
2019-04-14 01:23:27 +0800

    remove flow from index; got a little flow happy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-console */
-import { flow } from 'lodash';
 import createRandomBoard from './create-random-board';
 import formatBoard from './format-board/format-board';
 import chooseARandomNumberOfPieces from './choose-random-item';
 
 const format = process.argv[2];
 
-flow(
-  board => formatBoard(format, board),
-  console.log
-)(createRandomBoard(chooseARandomNumberOfPieces));
+const board = createRandomBoard(chooseARandomNumberOfPieces);
+const formattedBoard = formatBoard(format, board);
+
+console.log(formattedBoard);
