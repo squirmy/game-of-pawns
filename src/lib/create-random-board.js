f1e22cea5149b9a1344b2d077d3a964c7f46d8f3
Adam Woods <adam.tomato@gmail.com>
2019-04-29 19:41:23 +0800

    readability enhancements for create-random-board

--- src/lib/create-random-board.js
+++ src/lib/create-random-board.js
@@ -1,10 +1,12 @@
-import { flow, partial, times, range, shuffle } from 'lodash';
+import { flow, times, range, shuffle } from 'lodash';
 import chooseARandomSquare from './choose-random-item';
 import placePiece from './place-piece/place-piece';
 import createBoard from './create-board';
 
 const placePieceRandomly = (numberOfPieces, piece) =>
-  times(numberOfPieces, () => partial(placePiece, chooseARandomSquare, piece));
+  times(numberOfPieces, () => (...args) =>
+    placePiece(chooseARandomSquare, piece, ...args),
+  );
 
 export default chooseNumberOfPieces => {
   const placeOne = piece => placePieceRandomly(1, piece);
@@ -12,22 +14,22 @@ export default chooseNumberOfPieces => {
     return placePieceRandomly(chooseNumberOfPieces(range(0, max + 1)), piece);
   };
 
-  return flow(
-    shuffle(
-      [
-        placeOne('K'),
-        placeOne('k'),
-        placeUpTo(1, 'Q'),
-        placeUpTo(1, 'q'),
-        placeUpTo(8, 'P'),
-        placeUpTo(8, 'p'),
-        placeUpTo(2, 'R'),
-        placeUpTo(2, 'r'),
-        placeUpTo(2, 'N'),
-        placeUpTo(2, 'n'),
-        placeUpTo(2, 'B'),
-        placeUpTo(2, 'b'),
-      ].flatMap(x => x),
-    ),
-  )(createBoard('8/8/8/8/8/8/8/8'));
+  const piecePlacements = [
+    placeOne('K'),
+    placeOne('k'),
+    placeUpTo(1, 'Q'),
+    placeUpTo(1, 'q'),
+    placeUpTo(8, 'P'),
+    placeUpTo(8, 'p'),
+    placeUpTo(2, 'R'),
+    placeUpTo(2, 'r'),
+    placeUpTo(2, 'N'),
+    placeUpTo(2, 'n'),
+    placeUpTo(2, 'B'),
+    placeUpTo(2, 'b'),
+  ].flat();
+
+  const placePiecesInARandomOrder = flow(shuffle(piecePlacements));
+
+  return placePiecesInARandomOrder(createBoard('8/8/8/8/8/8/8/8'));
 };
